665828e14abb62c11d7f85ba818dbb1a75795ca1
c01d5t3w_l3n0v0 <coldstew@gmail.com>
2024-10-21 17:04:23 +0900

    Add project files.

---
+++ CoreEditor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CoreEditor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ CoreEditor/GameProject/ProjectBrowser.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoreEditor.GameProject
+{
+    /// <summary>
+    /// Interaction logic for ProjectBrowser.xaml
+    /// </summary>
+    public partial class ProjectBrowser : Window
+    {
+        public ProjectBrowser()
+        {
+            InitializeComponent();
+        }
+
+        private void OnToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == openProjectBtn) 
+            {
+                if(createProjectBtn.IsChecked == true)
+                {
+                    createProjectBtn.IsChecked = false;
+                    BrowserContent.Margin = new Thickness(0);
+                }
+                openProjectBtn.IsChecked = true;
+            }
+            else if(sender == createProjectBtn)
+            {
+                if (openProjectBtn.IsChecked == true)
+                {
+                    openProjectBtn.IsChecked = false;
+                    BrowserContent.Margin = new Thickness(-800,0,0,0);
+                }
+                createProjectBtn.IsChecked = true;
+            }
+        }
+    }
+}
