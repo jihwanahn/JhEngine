89581fb16db40a221c0063cc3953d82ecca92fb3
c01d5t3w_l3n0v0 <coldstew@gmail.com>
2024-10-23 22:10:23 +0900

    add proect templates / loading templates from browser

---
+++ CoreEditor/GameProject/NewProject.cs
@@ -0,0 +1,93 @@
+﻿using CoreEditor.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace CoreEditor.GameProject
+{
+    [DataContract]
+    public class ProjectTemplate
+    {
+        [DataMember] 
+        public string ProjectType { get; set; }
+        [DataMember] 
+        public string ProjectFile { get; set; }
+        [DataMember] 
+        public List<string> Folders{ get; set; }
+        [DataMember]
+        public byte[] Icon { get; set; }
+        [DataMember]
+        public byte[] Preview { get; set; }
+        [DataMember]
+        public string IconFilePath { get; set; }
+        [DataMember]
+        public string PreviewFilePath { get; set; }
+        [DataMember]
+        public string ProjectFilePath { get; set; }
+    }
+    class NewProject : ViewModelBase
+    {
+        private readonly string _templatePath = @"..\..\CoreEditor\ProjectTemplates";
+        //private readonly string _logPath = @"..\..\CoreEditor\Logs\";
+        private string _projectName = "NewProject";
+        public string ProjectName
+        {
+            get => _projectName;
+            set
+            {
+                if (_projectName != value)
+                {
+                    _projectName = value;
+                    OnPropertyChanged(nameof(ProjectName));
+                }
+            }
+        }
+        private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\AhnrealProject\";
+        public string ProjectPath
+        {
+            get => _projectPath;
+            set
+            {
+                if (_projectPath != value)
+                {
+                    _projectPath = value;
+                    OnPropertyChanged(nameof(ProjectPath));
+                }
+            }
+        }
+
+        private ObservableCollection<ProjectTemplate> _projectTemplates = new ObservableCollection<ProjectTemplate>();
+        public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates { get; }
+
+
+        public NewProject()
+        {
+            ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projectTemplates);
+            try {
+                var templateFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templateFiles.Any());
+                foreach (var templateFile in templateFiles) 
+                {
+                    var template = Serializer.FromFile<ProjectTemplate>(templateFile);
+                    template.IconFilePath = System.IO.Path.GetFullPath(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(templateFile), "Logo.png"));
+                    template.Icon = File.ReadAllBytes(template.IconFilePath);
+                    template.PreviewFilePath= System.IO.Path.GetFullPath(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(templateFile), "Preview.png"));
+                    template.Preview = File.ReadAllBytes(template.PreviewFilePath);
+                    template.ProjectFilePath = System.IO.Path.GetFullPath(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(templateFile), template.ProjectFile));
+
+                    _projectTemplates.Add(template);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Write(e.Message);
+                //File.WriteAllText(_logPath + "\\" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".txt", e.Message);
+            }
+        }
+    }
+}
---
+++ CoreEditor/Utilities/Serializer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreEditor.Utilities
+{
+    public static class Serializer
+    {
+        public static void ToFile<T>(T instance, string path)
+        {
+            try
+            {
+                using var fs = new FileStream(path, FileMode.Create);
+                var serializer = new DataContractSerializer(typeof(T));
+                serializer.WriteObject(fs, instance);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+
+        internal static T? FromFile<T>(string path)
+        {
+            try
+            {
+                using var fs = new FileStream(path, FileMode.Open);
+                var serializer = new DataContractSerializer(typeof(T));
+                return (T?)serializer.ReadObject(fs);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return default;
+            }
+        }
+    }
+}
